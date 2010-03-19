8b7966c8563385326f2357b093a72e109698cb30
Chris Burghart <burghart@ucar.edu>
2010-03-19 15:41:10 +0000

    o added tool_domx.py so that the domx tool will be built when required regardless of   where it lives in the source tree
    o SConscript now just "uses" the domx tool (effectively loading tool_domx.py)
    
    
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/domx@7375 da89c675-a4d7-0310-8a15-9e7546503515

---
+++ tool_domx.py
@@ -0,0 +1,45 @@
+# -*- python -*-
+
+import os
+
+tools = ['xercesc', 'logx', 'doxygen']
+env = Environment(tools = ['default'] + tools)
+
+domxDir = Dir('.').abspath
+
+def domx(env):
+    env.Append(LIBS=[env.GetGlobalTarget('libdomx'),])
+    env.AppendUnique(CPPPATH = domxDir)
+    env.Require (tools)
+
+Export('domx')
+
+sources = Split("""
+ XML.cc XmlObjectInterface.cc XmlObjectCatalog.cc XmlTime.cc XmlFileObject.cc
+ """)
+
+headers = Split("""
+ XML.h         XmlObjectCatalog.h    XmlObjectMember.h  XmlTime.h
+ XmlFileObject.h  XmlObjectInterface.h  XmlObjectNode.h
+ """)
+
+env.SourceCode('.', None)
+
+lib = env.Library('domx', sources)
+Default(lib)
+install_lib = env.InstallLibrary(lib)
+install_headers = env.InstallHeaders('domx', headers)
+
+xmlfilescan = env.Program('xmlfilescan', sources +
+              ["xmlfilescan.cc"])
+Default(xmlfilescan)
+env.InstallProgram(xmlfilescan)
+
+xmlcatalog = env.Program('xmlcatalog', sources +
+             ["xmlcatalog.cc"])
+Default(xmlcatalog)
+env.InstallProgram(xmlcatalog)
+
+SConscript(dirs=['tests'])
+
+env.Apidocs(sources + headers)
